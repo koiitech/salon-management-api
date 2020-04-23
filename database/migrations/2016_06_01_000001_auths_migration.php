6b98a3a7b93da1cb6750fb87dafc06bba6f910ec
Cảnh Kiều <canhkieu@MacBook-Air.local>
2020-04-23 11:59:58 -0700

    Tắt passport migration có sẵn

--- database/migrations/2016_06_01_000001_auths_migration.php
+++ database/migrations/2016_06_01_000001_auths_migration.php
@@ -13,50 +13,50 @@ class AuthsMigration extends Migration
      */
     public function up()
     {
-        // Schema::create('oauth_auth_codes', function (Blueprint $table) {
-        //     $table->string('id', 100)->primary();
-        //     $table->unsignedBigInteger('user_id')->index();
-        //     $table->unsignedBigInteger('client_id');
-        //     $table->text('scopes')->nullable();
-        //     $table->boolean('revoked');
-        //     $table->dateTime('expires_at')->nullable();
-        // });
+        Schema::create('oauth_auth_codes', function (Blueprint $table) {
+            $table->string('id', 100)->primary();
+            $table->uuid('user_id')->index();
+            $table->unsignedBigInteger('client_id');
+            $table->text('scopes')->nullable();
+            $table->boolean('revoked');
+            $table->dateTime('expires_at')->nullable();
+        });
 
-        // Schema::create('oauth_access_tokens', function (Blueprint $table) {
-        //     $table->string('id', 100)->primary();
-        //     $table->unsignedBigInteger('user_id')->nullable()->index();
-        //     $table->unsignedBigInteger('client_id');
-        //     $table->string('name')->nullable();
-        //     $table->text('scopes')->nullable();
-        //     $table->boolean('revoked');
-        //     $table->timestamps();
-        //     $table->dateTime('expires_at')->nullable();
-        // });
+        Schema::create('oauth_access_tokens', function (Blueprint $table) {
+            $table->string('id', 100)->primary();
+            $table->uuid('user_id')->nullable()->index();
+            $table->unsignedBigInteger('client_id');
+            $table->string('name')->nullable();
+            $table->text('scopes')->nullable();
+            $table->boolean('revoked');
+            $table->timestamps();
+            $table->dateTime('expires_at')->nullable();
+        });
 
-        // Schema::create('oauth_refresh_tokens', function (Blueprint $table) {
-        //     $table->string('id', 100)->primary();
-        //     $table->string('access_token_id', 100);
-        //     $table->boolean('revoked');
-        //     $table->dateTime('expires_at')->nullable();
-        // });
+        Schema::create('oauth_refresh_tokens', function (Blueprint $table) {
+            $table->string('id', 100)->primary();
+            $table->string('access_token_id', 100);
+            $table->boolean('revoked');
+            $table->dateTime('expires_at')->nullable();
+        });
 
-        // Schema::create('oauth_clients', function (Blueprint $table) {
-        //     $table->bigIncrements('id');
-        //     $table->unsignedBigInteger('user_id')->nullable()->index();
-        //     $table->string('name');
-        //     $table->string('secret', 100)->nullable();
-        //     $table->text('redirect');
-        //     $table->boolean('personal_access_client');
-        //     $table->boolean('password_client');
-        //     $table->boolean('revoked');
-        //     $table->timestamps();
-        // });
+        Schema::create('oauth_clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('user_id')->nullable()->index();
+            $table->string('name');
+            $table->string('secret', 100)->nullable();
+            $table->text('redirect');
+            $table->boolean('personal_access_client');
+            $table->boolean('password_client');
+            $table->boolean('revoked');
+            $table->timestamps();
+        });
 
-        // Schema::create('oauth_personal_access_clients', function (Blueprint $table) {
-        //     $table->bigIncrements('id');
-        //     $table->unsignedBigInteger('client_id');
-        //     $table->timestamps();
-        // });
+        Schema::create('oauth_personal_access_clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('client_id');
+            $table->timestamps();
+        });
     }
 
 
@@ -67,10 +67,10 @@ class AuthsMigration extends Migration
      */
     public function down()
     {
-        // Schema::dropIfExists('oauth_auth_codes');
-        // Schema::dropIfExists('oauth_access_tokens');
-        // Schema::dropIfExists('oauth_refresh_tokens');
-        // Schema::dropIfExists('oauth_clients');
-        // Schema::dropIfExists('oauth_personal_access_clients');
+        Schema::dropIfExists('oauth_auth_codes');
+        Schema::dropIfExists('oauth_access_tokens');
+        Schema::dropIfExists('oauth_refresh_tokens');
+        Schema::dropIfExists('oauth_clients');
+        Schema::dropIfExists('oauth_personal_access_clients');
     }
 }
